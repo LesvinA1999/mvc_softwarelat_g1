f9cf156ebf11f589d1c7463da6c98902d115a199
LesvinA1999 <79015243+LesvinA1999@users.noreply.github.com>
2022-07-29 22:42:52 -0600

    prueba

--- src/Controllers/Sec/Login.php
+++ src/Controllers/Sec/Login.php
@@ -2,6 +2,8 @@
 namespace Controllers\Sec;
 class Login extends \Controllers\PublicController
 {
+
+    //HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     private $txtEmail = "";
     private $txtPswd = "";
     private $errorEmail = "";
